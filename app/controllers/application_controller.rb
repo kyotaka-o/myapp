ed8da474d6064084f12cf8430378f606fca224f3
kyotaka-o <kyo-taka@docomo.ne.jp>
2019-07-20 11:51:00 +0900

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,9 @@ class ApplicationController < ActionController::Base
     @cube_elements = ["front","back","left","right","top","bottom"]
     @random_categories = Category.order("random()").limit(6)
     @categories = Category.all
+
+    ids= Post.group(:user_id).order('count_user_id DESC').limit(5).count(:user_id).keys
+    @ranking = ids.map{|user_id| User.find(user_id)}
+    @counts = ids.map{|id| Post.where(user_id:id).count}
   end
 end
