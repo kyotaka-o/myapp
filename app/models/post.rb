2d87000bc9ea8bd010624225da0aa80ab8b11413
kyotaka-o <kyo-taka@docomo.ne.jp>
2019-07-20 16:57:50 +0900

    update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  mount_uploaders :images, ImageUploader
+  mount_uploader :video, VideoUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_19_014006) do
+ActiveRecord::Schema.define(version: 2019_07_20_065915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2019_07_19_014006) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "status"
+    t.json "images"
+    t.string "video"
     t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
