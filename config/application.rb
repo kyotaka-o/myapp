81e6a024cb36d129ca0176648a1aaeb68a171044
kyotaka-o <kyo-taka@docomo.ne.jp>
2019-07-30 11:01:11 +0900

    update

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
-    config.autoload_paths += %W(#{config.root}/lib/)
+    config.paths.add 'lib', eager_load: true
+    # config.autoload_paths += %W(#{config.root}/lib/)
     config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
