948e7c51b1eaee1cb8813c92f50709cbbace645f
kyotaka-o <kyo-taka@docomo.ne.jp>
2019-07-31 20:22:44 +0900

    update depent

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
   belongs_to :status
-  has_many :comments
-  has_many :favorites
+  has_many :comments  , dependent: :destroy
+  has_many :favorites , dependent: :destroy
   has_many :users, through: :favorites
   mount_uploaders :images, ImageUploader
   mount_uploader :video, VideoUploader
